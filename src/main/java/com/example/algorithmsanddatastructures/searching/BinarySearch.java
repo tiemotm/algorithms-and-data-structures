800f17d2d6357ea9d7ea901217d016dcb3024c46
tiemotm <tiemo.timtschenko@gmx.de>
2021-12-10 19:49:04 +0100

    added BinarySearch

---
+++ src/main/java/com/example/algorithmsanddatastructures/searching/BinarySearch.java
@@ -0,0 +1,65 @@
+/**
+ * <h1>Implementation of binary search</h1>
+ * <p>
+ * An interval search algorithm for searching an element x in a sorted array A
+ * by splitting into two sub-arrays.
+ * Works with all data-types that implement the Comparable interface.
+ * Time complexity: O(log(n))
+ *
+ * @author  Tiemo Timtschenko
+ * @version 1.0
+ * @since   2021-12-10
+ *
+ * */
+
+package com.example.algorithmsanddatastructures.searching;
+
+public class BinarySearch {
+
+    /**
+     * Search element x in array arr from left to right
+     * @param arr This is the array where to search the element
+     * @param x This is the element to search
+     * @param left This is the left bound for the searching area
+     * @param right This is the right bound for the searching area
+     * @param <T> This describes the type parameter
+     * @return int This returns the index of the element x or the length of the array
+     */
+    public static <T extends Comparable<T>> int binarySearch(T[] arr, T x, int left, int right) {
+        if (left > right) {
+            throw new IllegalArgumentException("Left bound should be smaller or equal than the right bound");
+        }
+
+        // While left bound is smaller or equal to the right bound
+        // If not then x is not in arr
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            // If x is smaller than the element in the mid it must be in the right sub-array
+            if(arr[mid].compareTo(x) < 0) {
+                left = mid + 1;
+            }
+            // If x is greater than the element in the mid it must be in the left sub-array
+            else if(arr[mid].compareTo(x) > 0) {
+                right = mid - 1;
+            }
+            // Else it is x and the index is returned
+            else {
+                return mid;
+            }
+        }
+
+        return arr.length;
+    }
+
+    /**
+     * Search element x in whole array arr
+     * @param arr This is the array where to search the element
+     * @param x This is the element to search
+     * @param <T> This describes the type parameter
+     * @return int This returns the index of the element x or the length of the array
+     */
+    public static <T extends Comparable<T>> int binarySearch(T[] arr, T x) {
+        return binarySearch(arr, x, 0, arr.length - 1);
+    }
+}
---
+++ src/test/java/com/example/algorithmsanddatastructures/searching/BinarySearchTest.java
@@ -0,0 +1,58 @@
+package com.example.algorithmsanddatastructures.searching;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.example.algorithmsanddatastructures.searching.BinarySearch.binarySearch;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BinarySearchTest {
+
+    private Integer[] intArr = {0, 2, 5, 6, 10};
+    private String[] strArr = {"a", "d", "e", "g", "m"};
+
+    // Integer Tests
+    @Test
+    @DisplayName("Find element 5 in the integer array {0, 2, 5, 6, 10} at index 2")
+    public void testIntArrFind5atIndex2() {
+        int ret = binarySearch(intArr, 5);
+        assertThat(ret).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("Find element 10 in the integer array {0, 2, 5, 6, 10} at index 4")
+    public void testIntArrFind10atIndex4() {
+        int ret = binarySearch(intArr, 10);
+        assertThat(ret).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("Dont find element 11 in the integer array {0, 2, 5, 6, 10}, instead return length")
+    public void testIntArrDontFind11() {
+        int ret = binarySearch(intArr, 11);
+        assertThat(ret).isEqualTo(intArr.length);
+    }
+
+    // String Tests
+    @Test
+    @DisplayName("Find element \"d\" in the string array {\"a\", \"d\", \"e\", \"g\", \"m\"} at index 1")
+    public void testStringArrFindDatIndex1() {
+        int ret = binarySearch(strArr, "d");
+        assertThat(ret).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Find element \"m\" in the string array {\"a\", \"d\", \"e\", \"g\", \"m\"} at index 1")
+    public void testStringArrFindMatIndex4() {
+        int ret = binarySearch(strArr, "m");
+        assertThat(ret).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("Dont find element \"n\" in the string array {\"a\", \"d\", \"e\", \"g\", \"m\"}, instead return the length")
+    public void testStringArrDontFindN() {
+        int ret = binarySearch(strArr, "n");
+        assertThat(ret).isEqualTo(5);
+    }
+
+}
