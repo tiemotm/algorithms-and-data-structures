b740a3ad6cdac6a2e8f9fcb17f5d7be907738293
tiemotm <tiemo.timtschenko@gmx.de>
2021-12-10 19:50:12 +0100

    added InsertionSort

---
+++ src/main/java/com/example/algorithmsanddatastructures/sorting/InsertionSort.java
@@ -0,0 +1,37 @@
+/**
+ * <h1>Implementation of insertion sort</h1>
+ * <p>
+ * A sequential sort algorithm for sorting an array in ascending order
+ * by inserting the first element of the unsorted area
+ * into the right place of the sorted area.
+ * Works with all data-types that implement the Comparable interface.
+ * Time complexity: O(n^2)
+ *
+ * @author  Tiemo Timtschenko
+ * @version 1.0
+ * @since   2021-12-10
+ *
+ * */
+
+package com.example.algorithmsanddatastructures.sorting;
+
+public class InsertionSort {
+
+    /**
+     * Sorting arr in ascending order
+     * @param arr This is the arr that should be sorted
+     * @param <T> This describes the type parameter
+     */
+    public static <T extends Comparable<T>> void insertionSort(T[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            T key = arr[i];
+            int j = i - 1;
+
+            // move key as long as key is smaller than the current element in sorted area
+            while(j >= 0 && key.compareTo(arr[j]) < 0) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/example/algorithmsanddatastructures/sorting/InsertionSortTest.java
@@ -0,0 +1,28 @@
+package com.example.algorithmsanddatastructures.sorting;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.example.algorithmsanddatastructures.sorting.InsertionSort.insertionSort;
+import static com.example.algorithmsanddatastructures.sorting.SelectionSort.selectionSort;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InsertionSortTest {
+
+    private Integer[] intArr = {5, 2, 100, 32, 57};
+    private String[] strArr = {"h", "a", "j", "l", "o"};
+
+    @Test
+    @DisplayName("Sort integer array {5, 2, 100, 32, 57} into ascending order {2, 5, 32, 57, 100}")
+    public void testIntArraySortedAscending() {
+        insertionSort(intArr);
+        assertThat(intArr).isSorted();
+    }
+
+    @Test
+    @DisplayName("Sort string array {\"h\", \"a\", \"j\", \"l\", \"o\"} into ascending order {\"a\", \"h\", \"j\", \"l\", \"o\"}")
+    public void testStringArraySortedAscending() {
+        insertionSort(intArr);
+        assertThat(intArr).isSorted();
+    }
+}
